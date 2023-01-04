44091306f2c47d5727edf53954157efdee229736
Santos-Mateus <matteussbt@gmail.com>
2023-01-04 10:20:23 -0300

    trabalha tipos de variaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int valor = 20;
+        short valor2 = 5;
+        System.out.println(valor + valor2);        
+        
+        int numero = 10;
+        String numero2 = "2";
+        // A expressão "numero(int) + numero2(String)" concatena os valores das variáveis e não executam a operação matemática.  
+        System.out.println(numero + numero2);
     }
 }
