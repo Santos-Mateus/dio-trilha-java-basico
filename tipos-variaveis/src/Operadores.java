44dc374ccc186207e6aea4054bc766d448f803fd
Santos-Mateus <matteussbt@gmail.com>
2023-01-07 12:08:47 -0300

    inclui ternario.

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -10,7 +10,7 @@ public class Operadores {
         number = number * -1;
         System.out.println(number);
 
-        // Valores booleanos
+        //booleanos
         boolean simNao = false;
         System.out.println(simNao);
 
@@ -18,6 +18,35 @@ public class Operadores {
         
         simNao = !simNao; //Altera variável
         System.out.println(simNao);
+
+        //Ternário
+        int a, b;
+        a = 6;
+        b = 8;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+        int resultado2 = a == b ? 1 : 0; //Não se limita a String
+
+        System.out.println(resultado);
+        System.out.println(resultado2);
+
+        //Relacionais
+        String nomeUm = "Mateus";
+        String nomeDois = new String("Santos");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        //Lógicos &&, ||
+        
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+        
+
+        if(condicao1  && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }else{
+            System.out.println("Uma das condições é verdadeira");
+        }
     }
 }
       
